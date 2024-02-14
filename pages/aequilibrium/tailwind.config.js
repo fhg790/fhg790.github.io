6efcbb2dbb0af2ae1fe5fa4dc7790d474c61f45d
fhg790 <ml5244@hurra.com>
2024-02-14 10:29:25 +0100

    Aequilibrium -- adding animations on scroll

---
+++ pages/aequilibrium/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark_green": "rgb(74 157 156)",
+        "dark_gray": "rgb(114 114 114)",
+        "light_gray": "rgb(232 232 232)",
+        "light_gray_text": "rgb(167 167 167)",
+        "light_gray_bg": "rgb(242 242 242)"
+      }
+    },
+  },
+  plugins: [],
+}
+
